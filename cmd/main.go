ad73fd7cbca796610263c033f3457eff510a7724
Bumgu Kang <flrhyme9@gmail.com>
2025-05-11 12:38:06 +0900

    tailwind sucks

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/bonzonkim/play-with-gotth/internal/templates"
 )
 
+type Response struct {
+	Message string
+}
+
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/*", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello, world")
+		templates.Home().Render(r.Context(), w)
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		res := Response{
+			Message: "Hello, World",
+		}
+
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			log.Printf("Failed to encode response %v", err)
+		}
 	})
 
 	http.ListenAndServe(":8080", nil)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./internal/templates/*.templ", "internal/templates/*.go"],
+  content: ["./internal/templates/*.templ", "./internal/templates/*.go"],
   theme: { extend: {} },
   plugins: [],
 }
